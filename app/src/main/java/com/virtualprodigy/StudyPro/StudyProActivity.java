2cf6b38bbf8d5157db9f72f880dc3e2db07c2129
virtualprodigyllc <virtualprodigyllc@gmail.com>
2016-02-01 11:50:50 -0500

    Added the drawer but there's no fragments for the drawer items

--- app/src/main/java/com/virtualprodigy/StudyPro/StudyProActivity.java
+++ app/src/main/java/com/virtualprodigy/StudyPro/StudyProActivity.java
@@ -4,16 +4,20 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
-
 import com.virtualprodigy.studypro.Adapters.NavigationDrawerAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StudyProActivity extends AppCompatActivity {
     private final String TAG = this.getClass().getName().toString();
@@ -30,7 +34,8 @@ public class StudyProActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
 
-    private fragmentsEnum currentFragment = fragmentsEnum.studyTimer;
+    private Fragment currentFragment;
+    private fragmentsEnum currentFragEnum = fragmentsEnum.studyTimer;
 
     public enum fragmentsEnum {
         studyTimer,
@@ -45,27 +50,68 @@ public class StudyProActivity extends AppCompatActivity {
         context = this;
         res = context.getResources();
         ((StudyProApplication) getApplication()).getComponent().inject(this);
-//        intent = new Intent().setClass(this, CalcGrade.class);
-//        spec = tabHost.newTabSpec("gradeCalc").setIndicator("Grade Calculator",
-//                          res.getDrawable(R.drawable.ic_tab_calc))
-//                      .setContent(intent);
-//        tabHost.addTab(spec);
-//
-//        intent = new Intent().setClass(this, StudyTimer.class);
-//        spec = tabHost.newTabSpec("studytimer").setIndicator("Study Timer",
-//                          res.getDrawable(R.drawable.ic_tab_studytimer))
-//                      .setContent(intent);
-//        tabHost.addTab(spec);
-//
-//        intent = new Intent().setClass(this, CramSlamNotesListAdapter.class);
-//        spec = tabHost.newTabSpec("notes").setIndicator("notes",
-//                          res.getDrawable(R.drawable.ic_tab_notes))
-//                      .setContent(intent);
-//        tabHost.addTab(spec);
-//      tabHost.setCurrentTabByTag("studytimer");
-//
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navDrawer = (ListView) findViewById(R.id.left_drawer);
+        fragmentManager = getSupportFragmentManager();
+//        toolbar = getSupportActionBar();
+
+        setupNavDrawer();
     }
-    public void fin (){
-    	finish();
+
+    private void setupNavDrawer() {
+
+        navTitles = new ArrayList<String> (Arrays.asList(context.getResources().getStringArray(R.array.nav_items)));
+        navImages = context.getResources().obtainTypedArray(R.array.nav_icons);
+
+        navAdapter = new NavigationDrawerAdapter(context, navTitles, navImages);
+        navDrawer.setAdapter(navAdapter);
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_bar_open, R.string.nav_bar_close);
+        mDrawerToggle.syncState();
+        drawerLayout.setDrawerListener(mDrawerToggle);
+
+//        final Fragment premiumFrag = new PremiumFragment();
+//        final Fragment homepageFrag = new HomepageFragment();
+//        final StatpageFragment statpageFrag = new StatpageFragment();
+
+        //Add fragments
+//        fragmentManager.beginTransaction().add(R.id.DisplayFragment, currentSingleLayoutFrag, fragmentsEnum.homepage.toString()).addToBackStack(null).commitAllowingStateLoss();
+//        fragmentManager.beginTransaction().add(R.id.DisplayFragment, statpageFrag, fragmentsEnum.stats.toString()).addToBackStack(null).commitAllowingStateLoss();
+//        fragmentManager.beginTransaction().hide(statpageFrag).commitAllowingStateLoss();
+
+        //Set the default selection to the homepage
+        navDrawer.setItemChecked(1, true);
+        navDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                fragmentManager = getSupportFragmentManager();
+
+                switch (position) {
+                    case 0:
+//                        try {
+//                            Log.i(TAG, "Study Timer Opened");
+//                            currentFragEnum = fragmentsEnum.studyTimer;
+//                            fragmentManager.beginTransaction().hide(currentFragment)
+//                                    .show(fragmentManager.findFragmentByTag(fragmentsEnum.studyTimer.toString())).commitAllowingStateLoss();
+//                            currentFragment = fragmentManager.findFragmentByTag(fragmentsEnum.studyTimer.toString());
+//                            navDrawer.setItemChecked(position, true);
+//                        } catch (Exception e) {
+//                            Log.e(TAG, e.getMessage(), e);
+//                        }
+
+                        break;
+
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                }
+                drawerLayout.closeDrawers();
+            }
+        });
+
     }
+
 }
